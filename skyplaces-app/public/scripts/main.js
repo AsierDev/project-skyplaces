2623db4c6bdeb630d84edce87997a6e65eb647db
Alexa Spalato <alexaspalato@gmail.com>
2018-02-12 17:40:44 +0100

    test app.js

--- skyplaces-app/public/scripts/main.js
+++ skyplaces-app/public/scripts/main.js
@@ -1,40 +1,14 @@
-var map;
-var infowindow;
+'use strict';
 
-function initMap() {
-    var pyrmont = {lat: -33.867, lng: 151.195};
+class App extends Component {
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15
-    });
-
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: pyrmont,
-        radius: 500,
-        type: ['store']
-    }, callback);
-}
-
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
+    componentDidMount() {
+        placesApi.init()
+            .then(() => {
+                this.setState // TODO switch spinner off
+            })
+            .catch(console.error)        
     }
+
 }
 
-function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
-}
--- skyplaces-app/src/App.js
+++ skyplaces-app/src/App.js
@@ -1,19 +1,66 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import '../public/scripts/google-places-1.0.0'
 
 class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            inputType:'',
+            inputLocation:'',
+            places: []
+
+        }
+    }
+
+    componentDidMount() {
+        placesApi.init() // eslint-disable-line
+            .then(() => {
+                this.setState // TODO switch spinner off
+            })
+            .catch(console.error)
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <section className="hero uk-light">
+                <header className="uk-padding-small">
+                    <div className="uk-container uk-flex uk-flex-between">
+                        <h3 className="logo uk-text-uppercase">skyplaces</h3>
+                        <a href uk-icon="icon: heart; ratio: 2" />
+                    </div>
+                </header>
+                <div className="uk-container uk-container-small uk-align-center uk-padding-large">
+                    <h1 className="uk-text-center">Busca Restaurantes</h1>
+                    <form className="uk-grid-small " uk-grid>
+                        <div className="uk-width-1-2@m">
+                            <label htmlFor="select-food">Que quieres comer?</label>
+                            <select name className="uk-select" id="select-food">
+                                <option>ver todos los restaurantes</option>
+                                <option>chino</option>
+                                <option>tapas</option>
+                                <option>thailandes</option>
+                                <option>italiano</option>
+                                <option>thailandes</option>
+                                <option>japones</option>
+                                <option>tex-mex</option>
+                                <option>sushi</option>
+                            </select>
+                        </div>
+                        <div className="uk-width-1-2@m">
+                            <label htmlFor="choose-place">Donde?</label>
+                            <input type="text" id="choose-place" className="uk-input" placeholder="Indica ciudad o barrio" />
+                        </div>
+                    </form>
+                </div>
+            </section>
+            <section className="restaurantsgrid">
+                <div className="uk-container uk-block">
+                    <div className="uk-grid-width-1-3@m" uk-grid />
+                </div>
+            </section>
+        </div>
     );
   }
 }
