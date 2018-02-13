3279439993748c5b4fec0eaf0a40499c8f4bfd49
AsierDev <asierp.dev@gmail.com>
2018-02-13 11:47:11 +0100

    add inputs

--- skyplaces/src/App.js
+++ skyplaces/src/App.js
@@ -1,67 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-// import '../public/scripts/google-places-1.0.0'
+import Inputs from './components/inputs'
+
 
 class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            inputType:'',
-            inputLocation:'',
-            places: []
+  constructor() {
+    super()
+    this.state = {
+      inputType: '',
+      inputLocation: '',
+      places: []
 
-        }
     }
+  }
 
-    componentDidMount() {
-        placesApi.init() // eslint-disable-line
-            .then(() => {
-                this.setState // TODO switch spinner off
-            })
-            .catch(console.error)
-    }
+  componentDidMount() {
+    placesApi.init() // eslint-disable-line
+      .then(() => {
+        this.setState // TODO switch spinner off
+      })
+      .catch(console.error)
+  }
+
+  addLocation = input => {
+    this.setState({ inputLocation: input })
+  }
+
+  addType = input => {
+    this.setState({ inputType: input })
+  }
+
+  addType
+
+
+
   render() {
-    return (
-        <div>
-            <section className="hero uk-light">
-                <header className="uk-padding-small">
-                    <div className="uk-container uk-flex uk-flex-between">
-                        <h3 className="logo uk-text-uppercase">skyplaces</h3>
-                        <a href uk-icon="icon: heart; ratio: 2" />
-                    </div>
-                </header>
-                <div className="uk-container uk-container-small uk-align-center uk-padding-large">
-                    <h1 className="uk-text-center">Busca Restaurantes</h1>
-                    <form className="uk-grid-small " uk-grid>
-                        <div className="uk-width-1-2@m">
-                            <label htmlFor="select-food">Que quieres comer?</label>
-                            <select name className="uk-select" id="select-food">
-                                <option>ver todos los restaurantes</option>
-                                <option>chino</option>
-                                <option>tapas</option>
-                                <option>thailandes</option>
-                                <option>italiano</option>
-                                <option>thailandes</option>
-                                <option>japones</option>
-                                <option>tex-mex</option>
-                                <option>sushi</option>
-                            </select>
-                        </div>
-                        <div className="uk-width-1-2@m">
-                            <label htmlFor="choose-place">Donde?</label>
-                            <input type="text" id="choose-place" className="uk-input" placeholder="Indica ciudad o barrio" />
-                        </div>
-                    </form>
-                </div>
-            </section>
-            <section className="restaurantsgrid">
-                <div className="uk-container uk-block">
-                    <div className="uk-grid-width-1-3@m" uk-grid />
-                </div>
-            </section>
+    return <div>
+      <section className="hero uk-light">
+        <header className="uk-padding-small">
+          <div className="uk-container uk-flex uk-flex-between">
+            <h3 className="logo uk-text-uppercase">skyplaces</h3>
+            <a href uk-icon="icon: heart; ratio: 2" />
+          </div>
+        </header>
+        <div className="uk-container uk-container-small uk-align-center uk-padding-large">
+          <h1 className="uk-text-center">Busca Restaurantes</h1>
+
+          <Inputs onSubmitLocation={this.addLocation} onSubmitType={this.addType} />
+
         </div>
-    );
+      </section>
+      <section className="restaurantsgrid">
+        <div className="uk-container uk-block">
+          <div className="uk-grid-width-1-3@m" uk-grid />
+        </div>
+      </section>
+    </div>;
   }
 }
 
---
+++ skyplaces/src/components/inputs.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Inputs extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            inputLocation: "",
+            inputType: ""
+        }
+    }
+
+    keepInput = inputLocation => this.setState({ inputLocation })
+
+    handleChange = e => {
+        this.setState({ inputType: e.target.value });
+    }
+
+    submit = () => {
+
+        this.props.onSubmitLocation(this.state.inputLocation)
+        this.props.onSubmitType(this.state.inputType)
+
+        this.setState({ inputLocations: '' })
+
+    }
+
+    render() {
+        return <form className="uk-grid-small " uk-grid onSubmit={e => { e.preventDefault(); this.submit() }}>
+            <div className="uk-width-1-2@m">
+                <label htmlFor="select-food">Que quieres comer?</label>
+                <select name className="uk-select" id="select-food" value={this.state.value} onChange={this.handleChange}>
+                    <option>ver todos los restaurantes</option>
+                    <option value="chino">chino</option>
+                    <option value="tapas">tapas</option>
+                    <option value="thailandes">thailandes</option>
+                    <option value="italiano">italiano</option>
+                    <option value="griego">griego</option>
+                    <option value="japones">japones</option>
+                    <option value="tex-mex">tex-mex</option>
+                    <option value="sushi">sushi</option>
+                </select>
+            </div>
+
+            <div className="uk-width-1-2@m">
+                <label htmlFor="choose-place">Donde?</label>
+                <input type="text" id="choose-place" className="uk-input" placeholder="Indica ciudad o barrio" onChange={(e) => this.keepInput(e.target.value)} />
+            </div>
+        </form>
+    }
+
+}
+
+export default Inputs
